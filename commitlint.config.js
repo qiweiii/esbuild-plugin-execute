bda954c42f7c503ccbc3d70b90bd889529bcc179
qiweiii <yangqiwei97@gmail.com>
2021-12-15 16:51:13 +0800

    chore: setup project

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Whether commitlint uses the default ignore rules.
+   */
+  defaultIgnores: true,
+  /*
+   * Custom prompt configs
+   */
+  prompt: {
+    messages: {},
+    questions: {
+      type: {
+        description: 'please input type:',
+      },
+    },
+  },
+};
+
+module.exports = Configuration;
